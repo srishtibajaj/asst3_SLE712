f9409f2430187b2756c01b86933faea5b8f81574
srishtibajaj <50203608+srishtibajaj@users.noreply.github.com>
2019-06-01 03:35:59 +1000

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -4,7 +4,7 @@
 #formatdb -p F -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa 
 
 # incorporate some mismatches
-msbar -sequence 17.fa -count 100 -point 4 -block 0   -codon 0   -outseq mysample_mutated.fa
+msbar -sequence 17.fa -count 350 -point 4 -block 0   -codon 0   -outseq mysample_mutated.fa
 
 # run the blastn
 blast2 -m 8 -p blastn -e 0.001 -d Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa -i mysample_mutated.fa 
